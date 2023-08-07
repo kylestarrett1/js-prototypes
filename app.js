4c48b2e991470c88424748ef112679852f4fb9e1
kylestarrett1 <kylestarrett1@gmail.com>
2023-08-07 16:50:28 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+// class Person {
+//   name = 'Max';
+
+//   constructor() {
+//     this.age = 30;
+//   }
+
+//   greet() {
+//     console.log(`Hi I am ${this.name} and I am ${this.age} years old.`);
+//   }
+// }
+
+// const person = new Person();
+// person.greet();
+
+function Person() {
+  this.age = 30;
+  this.name = 'Max';
+  this.greet = function () {
+    console.log(`Hi I am ${this.name} and I am ${this.age} years old.`);
+  };
+}
+
+Person.prototype = {
+  printAge() {
+    console.log(this.age);
+  },
+};
+
+console.dir(Person);
+
+const p = new Person();
+p.greet();
+p.printAge();
+console.log(p.__proto__);
